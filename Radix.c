4f89e7b2f1c2e3ee801608763f5893699a32586f
Kalana <edata.incore@gmail.com>
2018-06-08 18:16:57 +0530

    insert function built complate  not test

--- Radix.c
+++ Radix.c
@@ -18,20 +18,20 @@ INTRUDUCTION :
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 typedef struct trienode TrieNode;
-TrieNode* createNode(char character);
+TrieNode* createNode(char * character);
 TrieNode *root ;
 char *sample_word;
 void FindLeaf(TrieNode * root_Nodes, TrieNode *Start_Node);
-//test Function
-void test_CreateNode();
-// end test function 
+int find(char *first_word, char *second_word);
 
-// Node struct  start-----
-typedef struct trienode{
+    // Node struct  start-----
+    typedef struct trienode
+{
     // for store charactor 
-    char data ;
+    char * data ;
     //  point to parent node
     TrieNode * parent ;
     // to point child node array
@@ -42,11 +42,12 @@ typedef struct trienode{
 }TrieNode;// node stract end ------
 
 //Create function start-----
-TrieNode* createNode(char character){
+TrieNode* createNode(char * word){
     //create new node 
     TrieNode* NewNode = (TrieNode*)malloc(sizeof(TrieNode));
-    NewNode->data = character;
-    NewNode->length=0;
+    char * new_word = malloc(sizeof(char)*(strlen(word)+1));
+    NewNode->data = strcpy(new_word, word);
+    NewNode->length = 0;
     NewNode->isLeaf= 1;
     for(int i=0;i<26;i++){
         NewNode->child[i]=NULL;
@@ -55,102 +56,84 @@ TrieNode* createNode(char character){
 }//create function end----
 
 //Insert function Start----
-void insertWord(char * word){
-    TrieNode * tmp_root = root;
-    TrieNode **tmp_child = root->child;
-    
-    while (*word != '\0'){ // check end charactor
+void insertWord(char * word,TrieNode * insert_node){
+    TrieNode * tmp_root = insert_node;
+    TrieNode **tmp_child = insert_node->child;
+    // while (*word != '\0'){ 
         int index =(int)tolower(*word)- (int)'a';
         if(tmp_child[index] == NULL){
-            // letter not in the tmp child 
-           tmp_child[index]=createNode(*word);
-           tmp_root->isLeaf=0;
-           tmp_child[index]->parent=tmp_root;
-           tmp_root=tmp_child[index];
-           tmp_child= tmp_root->child;
-           word ++;
+            tmp_child[index] = createNode(word);
+            printf("%p add %s\n", tmp_child[index], word);
+            tmp_root->isLeaf = 0;
+            tmp_child[index]->parent = tmp_root;
+
+            // break;
         }
         else{
-            // same letter has tree tmp child 
-            tmp_root = tmp_child[index];
-            tmp_child = tmp_root->child;
-            word++;
-        }
+            printf("to sparate %s\n", word);
+            // take data  
+            char * store_word = tmp_child[index]->data;
+            //choice what is the larage one  
+            char * long_word = strlen(store_word)>=strlen(word)? store_word: word;
+            char *short_word = strlen(store_word) >= strlen(word) ? word : store_word;
+            //find break point 
+            int break_index = find(long_word,short_word);
+            printf("%d  \n", break_index);
+            // allocate memory for word part 
+            char * word_1= malloc(sizeof(char)*break_index);
+            char *word_2 = malloc(sizeof(char) * (strlen(long_word) - break_index));
+            char *word_3=NULL;
+            //copy word part
+            memcpy(word_1, word, break_index);
+            char *word_to_child = malloc(sizeof(char) * break_index);
+            //set new value to the data 
+            tmp_child[index]->data = strcpy(word_to_child,word_1);
+            memcpy(word_2, &long_word[break_index], strlen(long_word) - break_index);
+            insertWord(word_2, tmp_child[index]);
+
+            // if break_index smaller then short word
+            if(break_index<strlen(short_word)){
+                word_3 = malloc(sizeof(char) * (strlen(short_word) - break_index));
+                memcpy(word_3, &short_word[break_index], strlen(short_word) - break_index);
+                insertWord(word_3,tmp_child[index]);
+            }
+            
+        //     break;
+        // }
        
     }
 }// End Insert function-----
-
-//print Suggestion 
-void printSuggestions( char * word  ){
-    sample_word = word;
-    TrieNode *tmp_root = root;
-    TrieNode **tmp_child = root->child;
-    int state=0;
-    while (*word != '\0')
-    { // check end charactor
-        int index = (int)tolower(*word) - (int)'a';
-        if (tmp_child[index] == NULL){
-            printf("No Suggestions for Your Input \n");
+int find(char * Long_word,char * short_word){
+    int index;
+    for(index=0;index<strlen(short_word);index++){
+        if(tolower(Long_word[index])!=tolower(short_word[index]))
             break;
-        }
-        tmp_root = tmp_child[index];
-        tmp_child = tmp_root->child;
-        word++;
-            
-        if (*word == '\0'){
-            printf("Suggession for Your input  \'%s\'\n",sample_word);
-            FindLeaf(tmp_root, tmp_root);
-        }   
     }
-    
-}//end print Suggesion 
-
-// print letter from Leaf_Node to startNode 
-void printWord( TrieNode *Start_Node, TrieNode *Leaf_Node)
-{
-    char word[50]="";
-    int i=0;
-    TrieNode * tmp_node = Leaf_Node;
-    while (tmp_node != Start_Node){
-        word[i] = tmp_node->data;
-        tmp_node = tmp_node->parent;
-        i++;
-    }
-
-    for(int j = --i; j>=0;j--){
-        printf("%c", word[j]);
-    }
+    return index;
 }
 
-// find leaf node to print word function
-void FindLeaf(TrieNode * Nodes,TrieNode *  Start_Node){
-    
-    TrieNode **t_child = Nodes->child;
-    for(int i=0 ; i<26 ;i++){
-        if (t_child[i]!=NULL)
-            FindLeaf(t_child[i], Start_Node);
-    }
-    if(Nodes->isLeaf==1){
-        printf("    >  %s",sample_word);
-        printWord(Start_Node, Nodes);
-        printf("\n");
-    }
-    
-}
-//end 
 
 //  main fuction ----------------------------------------------------
 int main(int argc, char *argv[])
 {
+    root = createNode("root");
+    
+    // char * word = "kalana";
+    // 
+   
+    // insertWord("year",root);
+    // insertWord("yard", root);
+    // insertWord("yesterday", root);
+
     //create root of tree 
-    root = createNode('r');
+   
     //read  file  and add word to the tree 
            char word[51];
            FILE *file;
            file = fopen("Sample Word List.txt", "r");
            if (file) {
                while (fscanf(file, " %50s", word) == 1){
-                   insertWord(word);
+                   insertWord(word,root);
                }
                 fclose(file);
             }
@@ -159,14 +142,14 @@ int main(int argc, char *argv[])
                 return 0;
             }
     //read file end
-    if(argc>1){
-        char *word = argv[1];
-        printSuggestions(word);
-    } 
-    else{
-        fprintf(stderr, "Please input word \n \t Usage :  %s  <Your Word > \n ", argv[0]);
-    }
-        
+    // if(argc>1){
+    //     char *word = argv[1];
+    //     printSuggestions(word);
+    // } 
+    // else{
+    //     fprintf(stderr, "Please input word \n \t Usage :  %s  <Your Word > \n ", argv[0]);
+    // }
+    
     return 0;
 }
 // end main function --------------------------------------------------
