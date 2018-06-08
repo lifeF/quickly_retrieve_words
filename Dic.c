49b0e7297c8b02800352b430718a02ae060e2dbd
Kalana <edata.incore@gmail.com>
2018-06-08 12:04:32 +0530

    crete and test insert function

--- Dic.c
+++ Dic.c
@@ -16,13 +16,16 @@ INTRUDUCTION :
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct trienode TrieNode;
 TrieNode* createNode(char character);
- TrieNode* root;
-//tree Root create
+TrieNode *root ;
+void Preorder(TrieNode *Nodes);
 
-//test Function 
+    //tree Root create
+
+    //test Function
     void test_CreateNode();
 // end test function 
 
@@ -32,8 +35,9 @@ typedef struct trienode{
     char data ;
     //  point to parent node
     TrieNode * parent ;
-    // to point child node array 
-    TrieNode * child;
+    // to point child node array
+    TrieNode* child[26];
+    
     // to hold number of child
     int length;
 }TrieNode;// node stract end ------
@@ -45,111 +49,104 @@ TrieNode* createNode(char character){
     TrieNode* NewNode = (TrieNode*)malloc(sizeof(TrieNode));
     NewNode->data = character;
     NewNode->length=0;
-    NewNode->parent=NULL;
-    NewNode->child=NULL;
+    for(int i=0;i<26;i++){
+        NewNode->child[i]=NULL;
+    }
+    
     return NewNode;
 }//create function end----
 
 //Insert function Start----
 void insertWord(char * word){
-    int Current_letter =0;
-    char temp_character=word[Current_letter];
-    TrieNode* temp_root= root; // to point root 
-    TrieNode* temp_child = root->child ; // to point temp root child
-    while(word[Current_letter]!='\0'){
-        int i=0, length = temp_root->length;
-
-        
-        
-        
-        //printf("%d\n",i);
-        while(i<length){
-           char node_char=temp_child->data;
-           if(temp_character==node_char){
-               printf("have same -> %c \n " ,word[Current_letter]);
-               Current_letter++;
-               temp_character=word[Current_letter];
-               if(temp_character=='\0')
-                    break;
-               temp_root=temp_child;
-               temp_child=temp_root->child;
-               i = 0; length = temp_root->length;
-           } 
-            temp_child++;
-            i++;
+    TrieNode * tmp_root = root;
+    TrieNode **tmp_child = root->child;
+    
+    while (*word != '\0'){ // check end charactor
+        int index =(int)tolower(*word)- (int)'a';
+        if(tmp_child[index] == NULL){
+           // printf("add %c\n", *word);
+           tmp_child[index]=createNode(*word);
+           tmp_root=tmp_child[index];
+           tmp_child= tmp_root->child;
+           word ++;
         }
-        if (i>=length)
-        {
-            temp_child=temp_root->child; 
-            int length = (temp_root->length);
-            TrieNode* child_List_point = (TrieNode*)malloc(sizeof(TrieNode)*(length+1));
-            TrieNode* child_List=child_List_point;
-            for(int k=0; k<length;k++){
-                child_List->data = temp_child->data;
-                child_List->length=(temp_child->length)+1;
-                child_List->parent=temp_root;
-                child_List->child=temp_child->child;
-                child_List++;
-                temp_child++;
-            }
-            printf("add -> %c \n " ,word[Current_letter]);
-            child_List= createNode(word[Current_letter]);
-            child_List->parent=temp_root;
-            child_List->length = 0;
-            child_List->child = NULL;
-            temp_root->child=child_List_point;
-            temp_root=child_List;
-            temp_child= temp_root->child;
-            Current_letter++;
+        else{
+           // printf("Have same  %c\n", *word);
+            tmp_root = tmp_child[index];
+            tmp_child = tmp_root->child;
+            word++;
         }
+       
     }
-//-------------
-    // TrieNode * tmp_root = root;
-    // TrieNode * tmp_child = root->child;
-    // printf("%p %p", tmp_root, root);
-    // while (*word != '\0'){ // check end charactor
-    //     while(){
-    //         if(!tmp_child)
-    //             break;
+}// End Insert function-----
+
+//print Sugession 
+void printSuggestions( char * word  ){
+    char * word_sample = word;
+    TrieNode *tmp_root = root;
+    TrieNode **tmp_child = root->child;
+    int state=0;
+    while (*word != '\0')
+    { // check end charactor
+        int index = (int)tolower(*word) - (int)'a';
+        if (tmp_child[index] == NULL){
+            printf("have No suggestions\n");
+            break;
+        }
+        tmp_root = tmp_child[index];
+        tmp_child = tmp_root->child;
+        word++;
             
-    //         tmp_child++;
-    //     }
-    //     TrieNode *New_Node = createNode(*word);
-    //     int length =tmp_root->length;
-    //     TrieNode* List = (TrieNode*)malloc(sizeof(TrieNode)*length+2);
+        if (*word == '\0'){
+            for (int i = 0; i < 26; i++)
+            {
+                if (tmp_child[i] != NULL){
+                    printf("%s", word_sample);
+                    Preorder(tmp_root);
+                    printf("\n");
         
-        
-    //     word++;
-    // }
-    // printf("\n");
-    // // }   
+                }
+            }      
+            
+            
+        }   
+    }
+    
+}
+//end print sugession 
 
-}// End Insert function----
-
+void Preorder(TrieNode * Nodes){
+    printf("%c",Nodes->data);
+    TrieNode **t_child = Nodes->child;
+    for(int i=0 ; i<26 ;i++){
+        if (t_child[i]!=NULL)
+            Preorder(t_child[i]);
+           
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     //create root of tree 
    root = createNode('r');
-    //test_CreateNode();
-    insertWord("kalana");
-    insertWord("kalana");
     //read  file 
-        //    int c;
-        //    FILE *file;
-        //    file = fopen("Sample Word List.txt", "r");
-        //    if (file) {
-        //         while ((c = getc(file)) != EOF)
-        //             putchar(c);
-        //         fclose(file);
-        //     }
-        //     else{
-        //         fprintf(stderr,"file connot read , or \" Sample Word List.txt\" file not exit ");
-        //     }
+           char word[51];
+           FILE *file;
+           file = fopen("Sample Word List.txt", "r");
+           if (file) {
+               while (fscanf(file, " %50s", word) == 1){
+                   insertWord(word);
+               }
+                fclose(file);
+            }
+            else{
+                fprintf(stderr,"file connot read , or \" Sample Word List.txt\" file not exit ");
+            }
     //read file end
     
     return 0;
 }
+
 
 // test function for test each function used in the source code 
 
---
+++ pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+    int * value =malloc(sizeof(int )*3) ;
+    int * new ;
+    return 0;
+}
+
+
